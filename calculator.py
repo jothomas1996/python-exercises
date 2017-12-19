8fa59ac138d5234273f91dba1ebf3dcc99512b7c
jothomas1996 <jothomas1996@gmail.com>
2017-12-19 10:28:46 +0530

    Documentation updates

--- calculator.py
+++ calculator.py
@@ -1,13 +1,17 @@
-# calculator script which can perform addition, substraction, division and multiplication
+""" calculator script which can perform addition, substraction, division and multiplication """
 
 def addition(input_1, input_2):
+    """ implimentation of addition """
     return input_1 + input_2
 
 def substraction(input_1, input_2):
+    """ implimentation of substraction """
     return input_1 - input_2
 
 def division(input_1, input_2):
+    """ implimentation of division """
     return input_1 / input_2
 
 def multiplication(input_1, input_2):
+    """ implimentation of multiplication """
     return input_1 * input_2
