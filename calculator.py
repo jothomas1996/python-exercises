8648dee39e75becbd5c9b329ee843a0e48843907
jothomas1996 <jothomas1996@gmail.com>
2017-12-19 09:53:03 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# calculator script which can perform addition, substraction, division and multiplication
+
+def addition(input_1, input_2):
+    return input_1 + input_2
+
+def substraction(input_1, input_2):
+    return input_1 - input_2
+
+def division(input_1, input_2):
+    return input_1 / input_2
+
+def multiplication(input_1, input_2):
+    return input_1 * input_2
