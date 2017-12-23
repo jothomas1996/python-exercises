4e7e2882de25ceba06246e492b40b8ae4e522fe3
jothomas1996 <jothomas1996@gmail.com>
2017-12-23 23:54:21 +0530

    rectification in click usage

--- calculator.py
+++ calculator.py
@@ -4,32 +4,45 @@ import click
 
 @click.group()
 def cli():
+    """ click group """
     pass
 
 @click.command()
-@click.option('--input_1', default = 0, help = 'First input value')
-@click.option('--input_2', default = 0, help = 'Second input value')
+@click.argument('input_1')
+@click.argument('input_2')
 def add(input_1, input_2):
     """ implimentation of addition """
-    print input_1 + input_2,
+    result = int(input_1) + int(input_2)
+    click.echo(result)
 
 @click.command()
-@click.option('--input_1', default = 0, help = 'First input value')
-@click.option('--input_2', default = 0, help = 'Second input value')
+@click.argument('input_1')
+@click.argument('input_2')
 def subtract(input_1, input_2):
     """ implimentation of substraction """
-    print input_1 - input_2,
+    result = int(input_1) - int(input_2)
+    click.echo(result)
 
 @click.command()
-@click.option('--input_1', default = 0, help = 'First input value')
-@click.option('--input_2', default = 0, help = 'Second input value')
-def division(input_1, input_2):
-    """ implimentation of division """
-    print input_1 / input_2,
-
-@click.command()
-@click.option('--input_1', default = 0, help = 'First input value')
-@click.option('--input_2', default = 0, help = 'Second input value')
+@click.argument('input_1')
+@click.argument('input_2')
 def multiply(input_1, input_2):
+    """ implimentation of division """
+    result = int(input_1) * int(input_2)
+    click.echo(result)
+
+@click.command()
+@click.argument('input_1')
+@click.argument('input_2')
+def divide(input_1, input_2):
     """ implimentation of multiplication """
-    print input_1 * input_2,
+    result = int(input_1) / int(input_2)
+    click.echo(result)
+
+cli.add_command(add)
+cli.add_command(subtract)
+cli.add_command(multiply)
+cli.add_command(divide)
+
+if __name__ == '__main__':
+    cli()
