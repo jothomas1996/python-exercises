6f17df7d89105c6410e4936e57afc16b595c250b
jothomas1996 <jothomas1996@gmail.com>
2018-01-05 00:38:53 +0530

    update
    Modifications to remove duplicate calculations

--- pandigital.py
+++ pandigital.py
@@ -4,6 +4,8 @@ will contain all digits from 1 to n at least once. An example for a 7
 pandigital number is 2135467.
 '''
 
+import sys
+
 def check_if_pandigital(string):
 	""" Check if value is pandigital with digits repeating exactly once"""
 	valueSet = set()
@@ -24,13 +26,19 @@ def cli():
 	""" Main function """
 	print 'Scanning for pandigital values'
 	calculationCount, pandigitalCount = 0, 0
-	for m in range(1000):
-		for n in range(10000):
-			string = str(m) + '*' + str(n) + '=' + str(m * n)
-			calculationCount += 1
-			if check_if_pandigital(string):
-				pandigitalCount += 1
-				print string
+	checked = []
+	for m in range(40):
+		for n in range(190):
+			#Check if this calculation has occured before
+			if set([m,n]) not in checked:
+				sys.stdout.write('\r'+'Calculation '+ str(calculationCount + 1))
+				sys.stdout.flush()
+				string = str(m) + '*' + str(n) + '=' + str(m * n)
+				checked.append(set([m,n]))
+				calculationCount += 1
+				if check_if_pandigital(string):
+					pandigitalCount += 1
+					print string
 	print '\nTotal number of calcuulations made : ', calculationCount
 	print 'Total number of Pandigitals found : ', pandigitalCount
 
